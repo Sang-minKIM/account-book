451d7a09b2068476c60cad8a334dd53d78240e9e
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2024-12-14 22:08:13 +0900

    refactor: use memoization for dailypayment

--- src/pages/payment-list/hooks/use-payment-list-view-model.ts
+++ src/pages/payment-list/hooks/use-payment-list-view-model.ts
@@ -1,3 +1,4 @@
+import { useCallback } from 'react'
 import { DateState } from '~/components/calender/calender.type'
 import { useCalender } from '~/components/calender/hooks/use-calender'
 import { SORT_ORDER } from '~/constants/query'
@@ -21,18 +22,21 @@ export const usePaymentListViewModel = () => {
   // Map을 배열로 변환합니다.
   const dailyPaymentArray = dailyPaymentMap ? Array.from(dailyPaymentMap.entries()) : []
 
-  const getDailyPayment = ({ type, year, month, day }: Pick<RequiredInfo, 'type'> & DateState) => {
-    const date = `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`
-    const payments = dailyPaymentMap.get(date)
-    return payments
-      ? payments.reduce((sum, payment) => {
-          if (payment.type === type) {
-            return sum + payment.amount
-          }
-          return sum
-        }, 0)
-      : 0
-  }
+  const getDailyPayment = useCallback(
+    ({ type, year, month, day }: Pick<RequiredInfo, 'type'> & DateState) => {
+      const date = `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`
+      const payments = dailyPaymentMap.get(date)
+      return payments
+        ? payments.reduce((sum, payment) => {
+            if (payment.type === type) {
+              return sum + payment.amount
+            }
+            return sum
+          }, 0)
+        : 0
+    },
+    [dailyPaymentMap]
+  )
 
   return {
     dailyPaymentList: dailyPaymentArray,
