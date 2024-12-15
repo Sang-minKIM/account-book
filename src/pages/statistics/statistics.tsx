f2362e1ecbee35b41e8561052c8a78922d3be0db
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2024-12-15 22:19:13 +0900

    feat: add legend for chart

--- src/pages/statistics/statistics.tsx
+++ src/pages/statistics/statistics.tsx
@@ -1,4 +1,6 @@
+import { styled } from 'styled-components'
 import { DonutChart } from './components/donut-chart'
+import { Flex, Text } from '@radix-ui/themes'
 
 export const Statistics = () => {
   const expenses = [
@@ -7,5 +9,30 @@ export const Statistics = () => {
     { label: '교통비', amount: 150 },
     { label: '유흥비', amount: 100 },
   ]
-  return <DonutChart data={expenses} />
+  expenses.sort((a, b) => b.amount - a.amount)
+  return (
+    <Flex direction="column" align="center" gap="1">
+      <DonutChart data={expenses} />
+      <Flex direction="column" gap="2">
+        {expenses.map((item, index) => (
+          <Legend key={item.label} label={item.label} color={`var(--accent-${11 - index})`} />
+        ))}
+      </Flex>
+    </Flex>
+  )
 }
+
+const Legend = ({ label, color }: { label: string; color: string }) => {
+  return (
+    <Flex align="center" gap="2">
+      <ColorBox color={color} />
+      <Text size="2">{label}</Text>
+    </Flex>
+  )
+}
+const ColorBox = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 2px;
+  background-color: ${({ color }) => color};
+`
