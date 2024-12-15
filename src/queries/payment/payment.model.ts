f360afcd478a38288b8ac145ce3059167ac08952
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2024-12-15 23:27:02 +0900

    fix: use join for category

--- src/queries/payment/payment.model.ts
+++ src/queries/payment/payment.model.ts
@@ -5,7 +5,7 @@ export const PAYMENTS_ENDPOINT = {
   list: (year: number, month: number, sortOrder: SortOrder) => {
     const startDate = `${year}-${month.toString().padStart(2, '0')}-01`
     const endDate = `${year}-${month.toString().padStart(2, '0')}-${new Date(year, month, 0).getDate()}`
-    return `${PAYMENTS_ENDPOINT.default}?date=gte.${startDate}&date=lte.${endDate}&order=date.${sortOrder}`
+    return `${PAYMENTS_ENDPOINT.default}?select=*,category:categories(*)&date=gte.${startDate}&date=lte.${endDate}&order=date.${sortOrder}`
   },
   detail: (paymentId: string) => {
     return `${PAYMENTS_ENDPOINT.default}?id=eq.${paymentId}&select=*,category:categories(*)&limit=1`
