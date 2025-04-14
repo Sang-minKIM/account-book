88c6ce97892c4b6c04513724b7f97833d4a2fb1f
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2025-04-15 00:10:05 +0900

    refactor: use fn pipelining for calculate date cell

--- src/components/calendar/calendar.tsx
+++ src/components/calendar/calendar.tsx
@@ -1,13 +1,13 @@
 import styled from 'styled-components'
-import { times } from 'es-toolkit/compat'
 
 import { TriangleLeftIcon, TriangleRightIcon } from '@radix-ui/react-icons'
 import { Flex, Grid as RGrid, IconButton, Text } from '@radix-ui/themes'
 
 import { getFirstDayOfMonth } from './services/get-first-day-of-month'
-import { getLastDayOfMonth } from './services/get-last-date-of-month'
+import { getLastDateOfMonth } from './services/get-last-date-of-month'
 import { Day } from './day'
 import { getDay } from './services/get-day'
+import { map, pipe, range, toArray } from '@fxts/core'
 
 interface CalendarDayInfo {
   date: number
@@ -29,6 +29,44 @@ interface CalendarProps {
 }
 
 export const Calendar = ({ year, month, dateCells }: CalendarProps) => {
+  const prevMonthDateCells = pipe(
+    range(getLastDateOfMonth(year, month - 1) - getFirstDayOfMonth(year, month), getLastDateOfMonth(year, month - 1)),
+    map((date) => {
+      return dateCells.prevMonthDate?.({
+        year,
+        month,
+        date,
+        day: getDay(year, month, date),
+      })
+    }),
+    toArray
+  )
+
+  const currentMonthDateCells = pipe(
+    range(FIRST_DATE_OF_MONTH, getLastDateOfMonth(year, month)),
+    map((date) => {
+      return dateCells.currentMonthDate({
+        year,
+        month,
+        date,
+        day: getDay(year, month, date),
+      })
+    }),
+    toArray
+  )
+
+  const nextMonthDateCells = pipe(
+    range(FIRST_DATE_OF_MONTH, WEEK_DAY_COUNT - getFirstDayOfMonth(year, month)),
+    map((date) => {
+      return dateCells.nextMonthDate?.({
+        year,
+        month,
+        date,
+        day: getDay(year, month, date),
+      })
+    }),
+    toArray
+  )
   return (
     <Container direction="column" width="100%" height="fit-content">
       <Flex justify="center" mb="2" align="center" py="2" gap="2">
@@ -56,47 +94,15 @@ export const Calendar = ({ year, month, dateCells }: CalendarProps) => {
       </Flex>
 
       <Grid columns="7" justify="center" align="center" gap="2" width="100%" height="100%">
-        {/* 이전 달의 날짜들 */}
-        {/* {pipe(range())} */}
-        {times(getFirstDayOfMonth(year, month), (index) => {
-          if (typeof dateCells.prevMonthDate === 'function') {
-            console.log(getLastDayOfMonth(year, month - 1))
-            const date = getLastDayOfMonth(year, month - 1) - getFirstDayOfMonth(year, month) + index + 1
-            return dateCells.prevMonthDate({
-              year,
-              month,
-              date,
-              day: getDay(year, month, date),
-            })
-          }
-        })}
-        {/* 현재 달의 날짜들 */}
-        {times(getLastDayOfMonth(year, month), (index) => {
-          const date = index + 1
-          return dateCells.currentMonthDate({
-            year,
-            month,
-            date,
-            day: getDay(year, month, date),
-          })
-        })}
-        {/* 다음 달의 날짜들 */}
-        {times(7 - getFirstDayOfMonth(year, month + 1), (index) => {
-          if (typeof dateCells.nextMonthDate === 'function') {
-            const date = index + 1
-            return dateCells.nextMonthDate({
-              year,
-              month,
-              date,
-              day: getDay(year, month, date),
-            })
-          }
-        })}
+        {prevMonthDateCells}
+        {currentMonthDateCells}
+        {nextMonthDateCells}
       </Grid>
     </Container>
   )
 }
-
+const FIRST_DATE_OF_MONTH = 1
+const WEEK_DAY_COUNT = 7
 const DAY_TITLES = ['일', '월', '화', '수', '목', '금', '토']
 
 const Container = styled(Flex)`
