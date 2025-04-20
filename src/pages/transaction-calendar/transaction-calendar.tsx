79ce782265fd89a29204883f4c1c4dc37065d9f0
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2025-04-20 13:22:30 +0900

    refactor: remove create-page link from transaction calendar

--- src/pages/transaction-calendar/transaction-calendar.tsx
+++ src/pages/transaction-calendar/transaction-calendar.tsx
@@ -1,8 +1,5 @@
-import { Container, Flex, Section, Text } from '@radix-ui/themes'
-import { Pencil1Icon } from '@radix-ui/react-icons'
+import { Flex, Section, Text } from '@radix-ui/themes'
 import { useTransactionCalendarViewModel } from './hooks/use-transaction-calendar-view-model'
-import { ROUTE } from '~/router'
-import { IconLink } from '~/components/base'
 import { Calendar } from '~/components/calendar'
 
 import { DailyTransactionList } from './components/daily-transaction-list'
@@ -54,27 +51,20 @@ export const TransactionCalendar = () => {
   }, [getDailyTransaction])
 
   return (
-    <Container>
-      <Flex height="50px" align="center" justify="end" p="2">
-        <IconLink to={ROUTE.calendar.create}>
-          <Pencil1Icon width="22" height="22" />
-        </IconLink>
-      </Flex>
-      <Flex justify="between" gap="9" height="calc(100dvh - 50px)">
-        <Section width="50%" minWidth="300px">
-          <Calendar year={year} oneBasedMonth={oneBasedMonth}>
-            <Calendar.Header
-              onPrevMonthClick={() => dispatchCalender({ type: ACTION_TYPE.SET_PREV_MONTH })}
-              onNextMonthClick={() => dispatchCalender({ type: ACTION_TYPE.SET_NEXT_MONTH })}
-            />
-            <Calendar.Body dateCells={dateCells} />
-          </Calendar>
-          <MonthlyTransaction monthlyIncome={monthlyIncome} monthlyExpense={monthlyExpense} />
-        </Section>
-        <Section width="50%" minWidth="300px">
-          <DailyTransactionList dailyTransactionList={dailyTransactionList} />
-        </Section>
-      </Flex>
-    </Container>
+    <Flex justify="between" gap="9" height="calc(100dvh - 50px)">
+      <Section width="50%" minWidth="300px">
+        <Calendar year={year} oneBasedMonth={oneBasedMonth}>
+          <Calendar.Header
+            onPrevMonthClick={() => dispatchCalender({ type: ACTION_TYPE.SET_PREV_MONTH })}
+            onNextMonthClick={() => dispatchCalender({ type: ACTION_TYPE.SET_NEXT_MONTH })}
+          />
+          <Calendar.Body dateCells={dateCells} />
+        </Calendar>
+        <MonthlyTransaction monthlyIncome={monthlyIncome} monthlyExpense={monthlyExpense} />
+      </Section>
+      <Section width="50%" minWidth="300px">
+        <DailyTransactionList dailyTransactionList={dailyTransactionList} />
+      </Section>
+    </Flex>
   )
 }
